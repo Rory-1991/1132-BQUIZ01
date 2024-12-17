f117a6b1f752dc8b0165c3d67451eb6b95a618a1
RoyLin <orangeskyonline@gmail.com>
2024-12-17 10:57:48 +0800

    修正分頁中當前頁須放大

--- front/main.php
+++ front/main.php
@@ -76,7 +76,9 @@ ww();
 
         </ul>
         <div id="altt"
-            style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+            style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); 
+            top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); 
+            background-position: initial initial; background-repeat: initial initial;">
         </div>
         <script>
             
--- front/news.php
+++ front/news.php
@@ -18,7 +18,7 @@
 				echo "<ol start='".($start+1)."'>";
 
                 foreach($rows as $row){
-                    echo "<li>";
+                    echo "<li class='sswww'>";
                     echo mb_substr($row['text'],0,20);
                         echo "<span class='all' style='display:none'>";
                         echo $row['text'];
@@ -40,7 +40,8 @@
                     echo "<a href='?do=$do&p=$prev'> < </a>";
                 }
                 for($i=1;$i<=$pages;$i++){
-                    echo "<a href='?do=$do&p=$i'> ";
+                    $size=($i==$now)?"24px":"16px";
+                    echo "<a href='?do=$do&p=$i' style='font-size:$size'> ";
                     echo $i;
                     echo " </a>";
                 }
@@ -53,12 +54,15 @@
             </div>
 </div>
 <div id="alt"
-	style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+	style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; 
+    text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; 
+    display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial 
+    initial; background-repeat: initial initial;">
 </div>
 <script>
 	$(".sswww").hover(
 		function() {
-			$("#alt").html("" + $(this).children(".all").html() + "").css({
+			$("#alt").html("<pre>" + $(this).children(".all").html() + "</pre>").css({
 				"top": $(this).offset().top - 50
 			})
 			$("#alt").show()
